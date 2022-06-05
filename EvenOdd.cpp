bbba0b583847cb72ec4631774f31848dfab2e2a4
Shiva Gupta <81801552+Shiva16-hub@users.noreply.github.com>
2022-06-05 09:57:18 +0530

    Create EvenOdd.cpp

---
+++ EvenOdd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Number is Even"<<endl;
+        
+    }
+    else
+    {
+        cout<<"Number is Odd"<<endl;
+    }
+    return 0;
+}
