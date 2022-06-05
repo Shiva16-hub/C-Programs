29feda2b4c97ffde286ef166f75d0457bdfdaeac
Shiva Gupta <81801552+Shiva16-hub@users.noreply.github.com>
2022-06-05 10:46:52 +0530

    Create incrementDecrement.cpp

---
+++ incrementDecrement.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   /* int i=1;
+        //10   //12
+    i = i++ + ++i;
+    cout<<i<<endl;
+    return 0; 
+    
+    output = 22
+    */
+    
+    
+/*    int i=1;
+    int j=2;
+    int k;
+    
+     //1  //2  //1    //2   //3   //4
+    k = i + j + i++ + j++ + ++i + ++j;
+    cout<<i<<" "<<j<<" "<<k;
+    
+    return 0;
+    
+    output = 13
+    */
+    
+/*    int i=0;
+    
+        //0   //0   //1   //0
+    i = i++ - --i + ++i - i--;
+        //1   //0   //1
+        
+        cout<<i<<endl;
+    
+    output = 0
+    */
+    
+ /*   int i=1, j=2, k=3;
+    
+            //1   //2    //3
+    int m = i-- - j-- - k--;
+    
+    cout<<i<<endl;      //0
+    cout<<j<<endl;      //1
+    cout<<k<<endl;      //2
+    cout<<m<<endl;
+    
+    Output = 1 , 1, 2, -4
+    
+   */ 
+   
+/*   int i=10, j=20, k;
+   
+      //10   //9   //19  //20  //9   //20  //10   //19
+   k = i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
+   
+   
+    cout<<i<<endl; //10   
+    cout<<j<<endl;   //20
+    cout<<k<<endl; 
+    
+    return 0;
+    
+    output = 10 , 20 , -20
+
+    */
+}
+
