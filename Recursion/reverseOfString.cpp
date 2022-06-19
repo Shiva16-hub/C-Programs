07ec850cfaaa3f6acf21a002a9a8947f30fc3ade
Shiva Gupta <81801552+Shiva16-hub@users.noreply.github.com>
2022-06-20 00:13:06 +0530

    Create reverseOfString.cpp

---
+++ Recursion/reverseOfString.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void revrse(string s){
+    if(s.length()==0){
+        return;
+    }
+    
+    string ros = s.substr(1);
+    revrse(ros);
+    cout<<s[0];
+}
+int main(){
+    revrse("shiva");
+}
