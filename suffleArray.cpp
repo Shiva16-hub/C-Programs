4c89857a0b27d296d699960f20ad9255dc4006e2
Shiva Gupta <81801552+Shiva16-hub@users.noreply.github.com>
2022-06-29 20:20:39 +0530

    Create suffleArray.cpp

---
+++ suffleArray.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Method to shuffle 2*n sized array's elements
+void shuffleArrayRecursive(int left, int right, int arr[]) { 
+	// Base case: return if 2 elements, no shuffle needed
+	if (right - left == 1) 
+		return; 
+
+	// get middle index of whole array
+	int middle = (left + right) / 2; 
+  
+	// get second half of first array 
+	int mmiddle = (left + middle) / 2; 
+
+	// get first half of second array 
+	int temp = middle + 1; 
+
+	// swapping elements for subarray
+	for (int i = mmiddle + 1; i <= middle; i++) 
+		swap(arr[i], arr[temp++]); 
+
+	// Pass divided first and second half to the 
+  // recursive routine
+	shuffleArrayRecursive(left, middle, arr); 
+	shuffleArrayRecursive(middle + 1, right, arr); 
+} 
+
+void shuffler(int a[], int n){
+	shuffleArrayRecursive(0, n-1, a);
+}
+
+int main() { 
+	int a[] = { 1, 3, 5, 7, 2, 4, 6, 8 }; 
+	int n = sizeof(a) / sizeof(a[0]); 
+	shuffler(a, n); 
+  
+	for (int i = 0; i < n; i++) 
+		cout << a[i] << " "; 
+  
+	return 0; 
+} 
